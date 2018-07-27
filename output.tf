0d9565654d7e5d64773801737f410ff3cee3c882
suresh <suresh@gmail.com>
2018-07-27 16:54:08 +0530

    my commit

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-# output "instance_ips" {
-#   value = ["${aws_instance.public-servers.*.public_ip}"]
-# }
-# output "ELB-DNS" {
-#   value = ["${aws_elb.bar.dns_name}"]
-# }
+ output "instance_ips" {
+   value = ["${aws_instance.public-servers.*.public_ip}"]
+ }
+ output "ELB-DNS" {
+   value = ["${aws_elb.bar.dns_name}"]
+ }
 
