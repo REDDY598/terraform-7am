5b9b43690f1dd543f037b0549a55c52d075fd342
MP Techno Solutions <41568630+devops-mptech@users.noreply.github.com>
2018-08-28 07:45:23 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,18 @@
    value = ["${aws_elb.bar.dns_name}"]
  }
 
+# variable "cidr1" {}
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "${var.cidr}"
+#   provisioner "local-exec" {
+#       command = "echo ${aws_vpc.myvpc.id} > ids.txt"
+#     }
+# }
+# resource "aws_subnet" "mysubnet" {
+#   vpc_id = "${aws_vpc.myvpc.id}"
+#   cidr_block = "${var.cidr1}"
+#   provisioner "local-exec" {
+#       command = "echo ${aws_subnet.mysubnet.id} >> ids.txt"
+#     }
+# }
+
